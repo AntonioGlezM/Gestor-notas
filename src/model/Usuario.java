cd0c66e81554ddf457e605f12c11f0c83464c05a
Antonio <antoniodawgc@gmail.com>
2026-03-08 14:52:30 +0000

    Añadido clase usuario y  carpeta data

---
+++ src/model/Usuario.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Usuario {
+
+    private String email;
+    private String passwordHash;
+
+    public Usuario(String email, String passwordHash) {
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+}
